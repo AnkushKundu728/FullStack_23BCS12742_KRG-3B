68cc3fcf17828abc9681413b60ed321cc42cb48d
AnkushKundu728 <ankushkundu905@gmail.com>
2025-11-17 20:06:19 +0530

    intial commit

--- Project/hostel_management_h2_v3/src/main/java/com/hostel/repository/FeeRepository.java
+++
@@ -1,9 +0,0 @@
-
-package com.hostel.repository;
-
-import com.hostel.model.FeePayment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FeeRepository extends JpaRepository<FeePayment, Long> {
-    FeePayment findByStudentId(Long studentId);
-}
