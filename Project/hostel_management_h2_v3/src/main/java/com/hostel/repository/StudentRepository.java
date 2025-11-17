88abeb9fded97ac7868d8dea2cdd51f548e272e3
AnkushKundu728 <ankushkundu905@gmail.com>
2025-11-17 19:47:03 +0530

    Delete Project directory

--- Project/hostel_management_h2_v3/src/main/java/com/hostel/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-
-package com.hostel.repository;
-
-import com.hostel.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
--- Project/hostel_management_h2_v3/src/main/java/com/hostel/service/NotFoundException.java
+++
@@ -1,6 +0,0 @@
-
-package com.hostel.service;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message){ super(message); }
-}
